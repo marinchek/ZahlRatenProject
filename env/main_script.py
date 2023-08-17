b4f34804efcefa587b65290eb39e522a8dd4c0b7
Marino Ivakovic <marino.ivakovic@rit.de>
2023-08-17 13:15:42 +0200

    neue function

--- env/main_script.py
+++ env/main_script.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
+ 
+# WSGI Application
+# Provide template folder name
+# The default folder name should be "templates" else need to mention custom folder name
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+ 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -12,7 +15,8 @@ def register():
     username = request.form['username']
     password1 = request.form['password1']
     password2 = request.form['password2']
-    
+    button = request.form['button']
+    print(button)
     # Add your function logic here to process the registration
     
     return "Registration successful!"  # Or redirect to another page
