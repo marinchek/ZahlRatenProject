4ea93a704cd8f7da788efc4358e937aa9da84bf0
pedda2803 <jonas.p2803@gmail.com>
2023-08-17 12:35:31 +0200

    added login site

--- env/main_script.py
+++ env/main_script.py
@@ -11,7 +11,7 @@ app = Flask(__name__, template_folder='templateFiles', static_folder='staticFile
  
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
  
 if __name__=='__main__':
     app.run(debug = True)
