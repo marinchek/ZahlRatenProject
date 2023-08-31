f1ca5411217de8106fc91a48cbb115057c2f35eb
Dillon Feld <98017494+dillon.feld@rit.de>
2023-08-31 09:42:57 +0200

    DB-Connection (WIP => Table-Erstellung macht noch Probleme)

--- env/database_requests.py
+++ env/database_requests.py
@@ -1,5 +1,7 @@
-import pyodbc 
+import pyodbc;
 import datetime;
+import mysql.connector;
+
 
 def CheckIfUserExists(username, password) -> bool:
     queryString = "SELECT * FROM accounts WHERE username = " + username + " AND password = " + password
@@ -41,17 +43,63 @@ def GetHighscoresFromDatabase():
 
 def MakeDbTransaction(transaction):
     GenerateDbConnectionAndCursor()
-    try:
-        returnMessage = transaction()
-        dbConnection.commit()
-    except:
-        returnMessage = "Couldn't commit transaction"
+    #try:
+    returnMessage = transaction()
+    dbConnection.commit()
+    #except:
+    returnMessage = "Couldn't commit transaction"
     dbConnection.close()
+    print(returnMessage)
     return returnMessage
     
     
 def GenerateDbConnectionAndCursor():
     global dbConnection 
-    dbConnection = pyodbc.connect('DRIVER={Devart ODBC Driver for SQLite};Server=127.0.0.1;Port=3306;Direct=True;Database=Guessing_game;String Types= Unicode')
+    #dbConnection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};ENCRYPT=no;SERVER=127.0.0.1;PORT=3306;Direct=True;DATABASE=Guessing_Game;UID=root')
+    dbConnection = mysql.connector.connect(host='localhost', user='root', password='')
+    
+    #connection = sqlite3.connect("database/db_luftwert.db")
+    #pointer = connection.cursor()
     global cursor 
     cursor = dbConnection.cursor()	
+
+
+def CreateDatabase():
+    createDatabaseString = """CREATE DATABASE IF NOT EXISTS Guessing_Game;"""
+
+    createAccountsTableString = """
+    USE Guessing_Game;
+    CREATE TABLE IF NOT EXISTS Accounts (
+        user_id int(11) NOT NULL AUTO_INCREMENT,
+        username varchar(64) NOT NULL,
+        password varchar(255) NOT NULL,
+        PRIMARY KEY (user_id)
+    )
+    """
+
+    createHighscoresTableString = """
+    CREATE TABLE IF NOT EXISTS Highscores (
+        score int(11) NOT NULL,
+        time_played int(11) NOT NULL,
+        timestamp datetime NOT NULL,
+        user_id varchar(64),
+        FOREIGN KEY user_id REFERENCES Accounts(user_id),
+        PRIMARY KEY (timestamp, user_id)
+    );"""
+    cursor.execute(createDatabaseString)
+    cursor.execute(createAccountsTableString)
+    cursor.execute(createHighscoresTableString)
+    return "Database created"
+
+MakeDbTransaction(CreateDatabase);
+
+"""" 
+#dbConnection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};ENCRYPT=no;SERVER=127.0.0.1;PORT=3306;Direct=True;DATABASE=Guessing_Game;UID=root')
+dbConnection = mysql.connector.connect(host='127.0.0.1', user='root', password='', database = 'test')
+
+#connection = sqlite3.connect("database/db_luftwert.db")
+#pointer = connection.cursor()
+cursor = dbConnection.cursor()	
+cursor.execute('SELECT * FROM artikel')
+print(cursor.fetchall())
+"""
