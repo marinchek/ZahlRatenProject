c6d865ce08528ef8a2a4388c1ac2fd138e8af896
Dillon Feld <98017494+dillon.feld@rit.de>
2023-08-31 14:14:51 +0200

    Alle Abfragen durchgetestet

--- env/database_requests.py
+++ env/database_requests.py
@@ -1,31 +1,43 @@
-import pyodbc;
-import datetime;
+from tkinter import INSERT
 import mysql.connector;
 
 
-def CheckIfUserExists(username, password) -> bool:
-    queryString = "SELECT * FROM accounts WHERE username = " + username + " AND password = " + password
-    cursor.execute(queryString) 
-    row_count = cursor.rowcount
+global dbConnection 
+global cursor 
+
+def CheckIfUserExists(userInfo) -> bool:
+    username = userInfo[0]
+    password = userInfo[1]
+    queryString = "SELECT * FROM accounts WHERE username = '" + username + "' AND password = '" + password + "'"
+    globals()["cursor"].execute(queryString) 
+    globals()["dbConnection"].commit()
+    row_count = globals()["cursor"].rowcount
     if row_count == 0:
         return False
     else:
         return True
     
 
-def RegisterUserToDatabase(username, password):
-    queryString = "INSERT INTO accounts (username, password) VALUES (" + username + ", " + password + ")"
-    cursor.execute(queryString) 
-    if cursor.rowcount != 0:
+def RegisterUserToDatabase(userInfo):
+    username = userInfo[0]
+    password = userInfo[1]
+    queryString = "INSERT INTO accounts (username, password) VALUES ('" + username + "', '" + password + "')"
+    globals()["cursor"].execute(queryString) 
+    globals()["dbConnection"].commit()
+    if globals()["cursor"].rowcount != 0:
         return "Register succesfull"
     else:
         return "Register Error"
 
-def SubmitHighscoreToDatabase(Username, Score):
-    cdt = datetime.datetime.now()
-    queryString = "INSERT INTO highscores (username, score, currentDateTime) VALUES (" + Username + ", " + Score + ", " + cdt + ")"
-    cursor.execute(queryString)
-    if cursor.rowcount != 0:
+def SubmitHighscoreToDatabase(accountInfo):
+    accountid = accountInfo[2]
+    time = accountInfo[1]
+    score = accountInfo[0]
+    #cdt = datetime.datetime.now()
+    queryString = "INSERT INTO Highscores (score, time_played, account_accountid) VALUES (" + str(score) + ", " + str(time) + ", " + str(accountid) + ")"
+    globals()["cursor"].execute(queryString)
+    globals()["dbConnection"].commit()
+    if globals()["cursor"].rowcount != 0:
         return "Submit succesfull"
     else:
         return "Submit Error"
@@ -33,37 +45,41 @@ def SubmitHighscoreToDatabase(Username, Score):
 
 def GetHighscoresFromDatabase():
     queryString = "SELECT * FROM highscores ORDER BY score DESC LIMIT 10"
-    cursor.execute(queryString) 
-    if cursor.rowcount != 0:
-        rows = cursor.fetchall()
+    globals()["cursor"].execute(queryString)
+    globals()["dbConnection"].commit() 
+    if globals()["cursor"].rowcount != 0:
+        rows = globals()["cursor"].fetchall()
         return rows
     else:
         return "Get Highscores Error"
 
 
-def MakeDbTransaction(transaction):
-    GenerateDbConnectionAndCursor()
+def MakeDbTransaction(transaction, *args):
     #try:
-    returnMessage = transaction()
-    dbConnection.commit()
+    if args:
+        returnMessage = transaction(*args)
+    else:
+        returnMessage = transaction()
     #except:
-    dbConnection.close()
+    #globals()["dbConnection"].close()
     print(returnMessage)
     return returnMessage
     
     
 def GenerateDbConnectionAndCursor():
-    global dbConnection 
     #dbConnection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};ENCRYPT=no;SERVER=127.0.0.1;PORT=3306;Direct=True;DATABASE=Guessing_Game;UID=root')
-    dbConnection = mysql.connector.connect(host='localhost', user='root', password='')
-    
-    #connection = sqlite3.connect("database/db_luftwert.db")
-    #pointer = connection.cursor()
-    global cursor 
-    cursor = dbConnection.cursor()	
+    globals()["dbConnection"] = mysql.connector.connect(host='localhost', user='root', password='')
+    globals()["cursor"] = globals()["dbConnection"].cursor(buffered=True)
+
+    goToDatabaseString = "USE Guessing_Game"	
+    globals()["cursor"].execute(goToDatabaseString )
+
 
 
 def CreateDatabase():
+    #dbConnection = mysql.connector.connect(host='localhost', user='root', password='')
+    
+    #cursor = globals()["dbConnection"].cursor()
     createDatabaseString = """CREATE DATABASE IF NOT EXISTS Guessing_Game;"""
 
     goToDatabseString = """USE Guessing_Game"""
@@ -87,25 +103,124 @@ def CreateDatabase():
         PRIMARY KEY (time_played, account_accountid)
     )"""
 
-    cursor.execute(createDatabaseString)
-    cursor.execute(goToDatabseString)
-    cursor.execute(createAccountsTableString)
-    cursor.execute(createHighscoresTableString)
+    globals()["cursor"].execute(createDatabaseString)
+    globals()["cursor"].execute(goToDatabseString)
+    globals()["cursor"].execute(createAccountsTableString)
+    globals()["cursor"].execute(createHighscoresTableString)
+    
     return "Database created"
 
-#MakeDbTransaction(CreateDatabase)
-
-#Create he Database & Tables in MySQL
-# MakeDbTransaction(CreateDatabase)
 
 
-"""" 
-#dbConnection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};ENCRYPT=no;SERVER=127.0.0.1;PORT=3306;Direct=True;DATABASE=Guessing_Game;UID=root')
-dbConnection = mysql.connector.connect(host='127.0.0.1', user='root', password='', database = 'test')
+def FillAccountTable():
+    insertIntoAccountsString = """
+    INSERT INTO Accounts (username, password)
+    VALUES 
+    ('test', 'test'),
+    ('Dillon', 'Feld'),
+    ('Marino', 'Ivakovic'),
+    ('Levent', 'Mutlu'),
+    ('Leon', 'Graf')
+    """
 
-#connection = sqlite3.connect("database/db_luftwert.db")
-#pointer = connection.cursor()
-cursor = dbConnection.cursor()	
-cursor.execute('SELECT * FROM artikel')
-print(cursor.fetchall())
-"""
+    globals()["cursor"].execute(insertIntoAccountsString)
+    globals()["dbConnection"].commit() 
+    return "Accounts filled"
+
+
+
+def FillHighscoreTable():
+    
+    insertIntoHighscoresString = """
+    INSERT INTO Highscores (score, time_played, account_accountid)
+    VALUES 
+    (100, 100, 131),
+    (200, 200, 132),
+    (300, 300, 133),
+    (400, 400, 134)
+    """
+    globals()["cursor"].execute(insertIntoHighscoresString)
+    globals()["dbConnection"].commit() 
+    return "Highscores filled"
+
+
+
+def ClearDataHighscores():
+    deleteHighscoresString = """
+    DELETE FROM Highscores
+    """
+    globals()["cursor"].execute(deleteHighscoresString)
+    globals()["dbConnection"].commit() 
+
+    return "Highscores cleared"
+
+
+
+def ClearDataAccounts():
+    deleteAccountsString = """
+    DELETE FROM Accounts
+    """
+    globals()["cursor"].execute(deleteAccountsString)
+    globals()["dbConnection"].commit() 
+
+    return "Accounts cleared"
+
+
+
+def TestAccountVerification():
+    assert CheckIfUserExists(("test", "test"))
+    assert CheckIfUserExists(("Dillon", "Feld"))
+    assert CheckIfUserExists(("Marino", "Ivakovic"))
+    assert CheckIfUserExists(("Levent", "Mutlu"))
+    assert CheckIfUserExists(("Leon", "Graf"))
+    assert not CheckIfUserExists(("test", "test1"))
+    assert not CheckIfUserExists(("test", "Feld"))
+    assert not CheckIfUserExists(("test", "Dillon"))
+    assert not CheckIfUserExists(("test", "Leon"))
+    assert not CheckIfUserExists(("Leon", "test"))
+
+    return "Account Verification Test Succesfull"
+
+
+
+def TestDataAccess():
+    assert MakeDbTransaction(GetHighscoresFromDatabase) != "Get Highscores Error"
+    assert MakeDbTransaction(RegisterUserToDatabase, ("Hans", "Peter")) == "Register succesfull"
+    assert MakeDbTransaction(SubmitHighscoreToDatabase, (5, 10, 135)) == "Submit succesfull"
+    assert MakeDbTransaction(CheckIfUserExists, ("Hans", "Peter"))
+    
+    return "Data Access Test Succesfull"
+
+
+
+def TestClearData():
+    message = MakeDbTransaction(ClearDataHighscores)+ "\n"
+    message += MakeDbTransaction(ClearDataAccounts) 
+    return message
+
+
+
+def SetupDatabase():
+    
+    GenerateDbConnectionAndCursor()
+    #Create the Database & Tables in MySQL
+    #CreateDatabase()
+    TestClearData()
+
+    #Fill the Database with some Data
+    message = MakeDbTransaction(FillAccountTable) + "\n"
+    message += MakeDbTransaction(FillHighscoreTable)+ "\n"
+
+    #Check if the Account Verification works
+    message += MakeDbTransaction(TestAccountVerification)+ "\n"
+
+    #Check if the data can be accessed
+    message += MakeDbTransaction(TestDataAccess)+ "\n"
+
+    #Delete the Dummy-Data
+    message += TestClearData()
+    #return message
+    return message
+
+message = SetupDatabase()
+print(message)
