e98a1c34237f2895fb9033ee6810dafddf5ac566
pedda2803 <jonas.p2803@gmail.com>
2023-08-31 12:02:47 +0200

    Fixed error when no input

--- env/main_script.py
+++ env/main_script.py
@@ -164,8 +164,8 @@ def game():
             print(session['randomNumber'])
     
     if request.method == 'POST':
-        user_guess = int(request.form.get('user_guess', 0))
-        if user_guess == 0 or user_guess == "0" or user_guess == "" or user_guess == None:
+        user_guess = request.form.get('user_guess', 0)
+        if user_guess == "0" or user_guess == "" or user_guess == None:
             number_guess = "You have not entered any number!"
         elif user_guess > max_range or user_guess < min_range:
             number_guess = "Your number is not in range!"
